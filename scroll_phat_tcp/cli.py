a1c5a3ae10d483409812840677c964adafaa9e4b
azcoigreach <azcoigreach@gmail.com>
2019-08-14 11:54:07 -0700

    updated logging

--- scroll_phat_tcp/cli.py
+++ scroll_phat_tcp/cli.py
@@ -49,11 +49,11 @@ def cli():
     s.listen(1)
 
     conn, addr = s.accept()
-    print 'Connection address:', addr
+    logger.INFO('Connection address:', addr)
     while 1:
         data = conn.recv(BUFFER_SIZE)
         if not data: break
-        print "received data:", data
+        logger.INFO("received data:", data)
         if data == 'plasma':
             plasma()
         else:
